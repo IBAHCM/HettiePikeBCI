1b94efc4fe436a80544c9bd20a2747be4210b090
hettiepike <2576341P@student.gla.ac.uk>
2021-02-12 15:15:52 +0000

    data acknowledgments

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' @title Barro Colorado Forest Census Plot Data
+#'
+#' full census data
+#'
+#' @source \url{https://repository.si.edu/bitstream/handle/10088/20925/ViewFullTable.zip?sequence=17&isAllowed=y}
+#' taxonomic data
+#' @source \url{https://repository.si.edu/bitstream/handle/10088/20925/ViewTax.txt?sequence=4&isAllowed=y}
